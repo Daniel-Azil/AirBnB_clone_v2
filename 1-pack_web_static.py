9dde82683d9babd9946b66ad1d505641aa0920e3
Daniel Azil <danielazil.fse@gmail.com>
2024-02-05 21:22:14 +0000

    A module that creates a gzip file of web_static dir in the versions dir.

---
+++ 1-pack_web_static.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+"""
+    A python module that utilizes fabric to create gzip
+    of specified directory.
+"""
+
+from datetime import datetime 
+from fabric.api import local
+from os.path import isdir
+
+
+def do_pack():
+    """
+        A function that creates versions of updated static pages
+        and gzip of the webstatic directory.
+    """
+    try:
+        current_datetime = datetime.now().strftime("%Y%m%d%H%M%S")
+        if isdir("versions") is False:
+            local("mkdir versions")
+        zip_filename = "web_static_{}.tgz".format(current_datetime)
+        archive_path = "versions/{}".format(zip_filename)
+        local("tar -czvf {} web_static".format(archive_path))
+        return archive_path
+
+    except:
+        return None
