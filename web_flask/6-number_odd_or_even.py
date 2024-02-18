0452a0a940ec1d41ca8c5a7edfea3f4e66a858ac
Daniel Azil <danielazil.fse@gmail.com>
2024-02-18 20:45:04 +0000

    Update routes and html template

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -38,9 +38,15 @@ def number_template_route(n):
     return render_template('5-number.html', n=n)
 
 
-@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
-def even_or_odd_integer(n):
-    return render_template('templates/6-number_odd_or_even.html', n=n)
+@app.route('/number_odd_or_even/<int:n>')
+def html_odd_or_even(n):
+    """display html page only if int given
+       place given int into html template
+       substitute text to display if int is odd or even
+    """
+    odd_or_even = "even" if (n % 2 == 0) else "odd"
+    return render_template('6-number_odd_or_even.html',
+                           n=n, odd_or_even=odd_or_even)
 
 
 if __name__ == '__main__':
