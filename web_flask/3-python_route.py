5c14c49b291d876ff6cd34652341aa2ddcea91e5
Daniel Azil <danielazil.fse@gmail.com>
2024-02-18 18:15:40 +0000

    Adhere to pycodestyle.

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -21,6 +21,7 @@ def hbnb_route():
 def c_route(text):
     return 'C {}'.format(text.replace('_', ' '))
 
+
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python_route(text='is cool'):
